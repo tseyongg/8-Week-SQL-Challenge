e14b18d781d993f6d4882000d007049407073582
tseyongg <tseyong0987@gmail.com>
2023-06-14 17:55:36 +0800

    edited

--- Project #2 - Pizza Runner/Proj2_Final.sql
+++ Project #2 - Pizza Runner/Proj2_Final.sql
@@ -7,21 +7,55 @@
 --Tools used: MS SQL Server, VSCode 
 
 WITH customer_orders_cte AS
-    (SELECT
-        order_id,
-        customer_id,
-        pizza_id,
-        CASE 
-  	        WHEN exclusions IS NULL OR exclusions LIKE 'null' THEN ''
-    	    ELSE exclusions 
-    	    END AS exclusions,
-        CASE 
-  	        WHEN extras IS NULL OR extras LIKE 'null' THEN ''
-    	    ELSE extras 
-    	    END AS extras,
-        order_time
-     FROM customer_orders
-    )
+(
+  SELECT
+    order_id,
+    customer_id,
+    pizza_id,
+    CASE 
+  	  WHEN exclusions IS NULL OR exclusions LIKE 'null' THEN ''
+      ELSE exclusions 
+      END AS exclusions,
+    CASE 
+  	  WHEN extras IS NULL OR extras LIKE 'null' THEN ''
+      ELSE extras 
+      END AS extras,
+    order_time
+  FROM customer_orders
+)
 
 SELECT *
-FROM customer_orders_cte
+FROM customer_orders_cte;
+
+
+
+WITH runner_orders_cte AS
+(
+  SELECT 
+    order_id, 
+    runner_id,  
+    CAST(
+	  CASE WHEN pickup_time LIKE 'null' THEN NULL
+	  ELSE pickup_time
+	  END AS DATETIME) AS pickup_time,
+    CAST(
+      CASE
+	  WHEN distance LIKE 'null' THEN NULL
+	  WHEN distance LIKE '%km' THEN TRIM('km' from distance)
+	  ELSE distance 
+      END AS FLOAT) AS distance,
+    CAST(
+      CASE WHEN duration LIKE 'null' THEN NULL
+	  WHEN duration LIKE '%mins' THEN TRIM('mins' from duration)
+	  WHEN duration LIKE '%minute' THEN TRIM('minute' from duration)
+	  WHEN duration LIKE '%minutes' THEN TRIM('minutes' from duration)
+	  ELSE duration
+	  END AS INT) AS duration,
+    CASE WHEN cancellation IN ('null', '') THEN NULL
+	  ELSE cancellation
+	  END AS cancellation
+  FROM runner_orders
+)
+
+SELECT *
+FROM runner_orders_cte;
